07aa27294c5a670727aa9365b9448d5290506420
homura <sangn2911@gmail.com>
2025-11-18 13:04:23 +0700

    update script, binary file

--- alwaysdata/build_binary.sh
+++ alwaysdata/build_binary.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm alwaysdata/shortlink
 docker build -f alwaysdata/Dockerfile.binary -t shortlink:latest .
 container_id=$(docker create shortlink:latest)
 docker cp $container_id:/app/shortlink ./alwaysdata/shortlink
